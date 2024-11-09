d648d6108b86353b06698c4ad4c11e32b35033d2
bernd3k <58431264+bernd3k@users.noreply.github.com>
2024-11-09 17:24:55 +0100

    style: incorporate more of Google's shell styleguide
    e.g. constant use of curly braces and quoting for variables, capitalization
    of names of code variables, constants, environment variables, etc.
    also linting via SpellCheck
    
    Refer-to: https://google.github.io/styleguide/shellguide.html
    Refer-to: https://www.shellcheck.net/

--- scripts/pre-consumption/decrypt-pdf/decrypt-pdf.sh
+++ scripts/pre-consumption/decrypt-pdf/decrypt-pdf.sh
@@ -15,31 +15,39 @@
 # DOCUMENT_SOURCE_PATH 	Original path of the consumed document
 # DOCUMENT_WORKING_PATH 	Path to a copy of the original that consumption will work on
 # TASK_ID 	UUID of the task used to process the new document (if any)
+
 SCRIPT_PATH=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-SCRIPT_NAME=$(basename "$SCRIPT_PATH")
-CONSUMABLE=$(basename "$DOCUMENT_WORKING_PATH")
+readonly SCRIPT_PATH
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+readonly SCRIPT_DIR
+SCRIPT_NAME=$(basename "${SCRIPT_PATH}")
+readonly SCRIPT_NAME
+CONSUMABLE=$(basename "${DOCUMENT_WORKING_PATH}")
+readonly CONSUMABLE
 
-printf -- '--- %(%F %H:%M:%S)T | %s -------------------------------------\n' -1 ${SCRIPT_NAME}
+printf -- '--- %(%F %H:%M:%S)T | %s -------------------------------------\n' -1 "${SCRIPT_NAME}"
 
-PWD_CORPUS="$(cat $(find ${SCRIPT_DIR} -type f -iname '*.pwd.txt') | sort -u)"
-PWD_COUNT="$(printf '%s' "$PWD_CORPUS" | wc -l | cut -f 1 -d ' ')"
-printf 'password corpus with %u entries assembled\n' ${PWD_COUNT}
+# shellcheck disable=SC2046
+PWD_CORPUS=$(cat $(find "${SCRIPT_DIR}" -type f -iname '*.pwd.txt') | sort -u)
+readonly PWD_CORPUS
+PWD_COUNT=$(printf '%s' "${PWD_CORPUS}" | wc -l | cut -f 1 -d ' ')
+readonly PWD_COUNT
+printf 'password corpus with %u entries assembled\n' "${PWD_COUNT}"
 
-if qpdf --is-encrypted "$DOCUMENT_WORKING_PATH"; then
-    DECODED=0
-    printf '%s is encrypted, trying to decrypt... ' ${CONSUMABLE}
+if qpdf --is-encrypted "${DOCUMENT_WORKING_PATH}"; then
+    decoded=0
+    printf '%s is encrypted, trying to decrypt... ' "${CONSUMABLE}"
     
-    while IFS= read -r pwd_line || [[ -n $pwd_line ]]; do
+    while IFS= read -r pwd_line || [[ -n "${pwd_line}" ]]; do
     
-        qpdf --requires-password --password=${pwd_line} ${DOCUMENT_WORKING_PATH}
+        qpdf --requires-password --password="${pwd_line}" "${DOCUMENT_WORKING_PATH}"
         
         # exit codes: see https://qpdf.readthedocs.io/en/stable/cli.html#option-requires-password
         case $? in
             3)  
-                qpdf --decrypt --password=${pwd_line} ${DOCUMENT_WORKING_PATH} --replace-input
+                qpdf --decrypt --password="${pwd_line}" "${DOCUMENT_WORKING_PATH}" --replace-input
                 printf 'decrypted\n'
-                DECODED=1
+                decoded=1
                 
                 printf 'password reminder:\n'
                 case ${#pwd_line} in
@@ -48,11 +56,11 @@ if qpdf --is-encrypted "$DOCUMENT_WORKING_PATH"; then
                         ;;
                         
                     3-5)
-                        printf 'starts with %s and ends with %s\n' ${pwd_line: 0: 1} ${pwd_line: -1}
+                        printf 'starts with %s and ends with %s\n' "${pwd_line: 0: 1}" "${pwd_line: -1}"
                         ;;
                         
                     *)
-                        printf 'has %u characters, starts with %s and ends with %s\n' ${#pwd_line} ${pwd_line: 0: 1} ${pwd_line: -1}
+                        printf 'has %u characters, starts with %s and ends with %s\n' "${#pwd_line}" "${pwd_line: 0: 1}" "${pwd_line: -1}"
                         ;;
                 esac
                 
@@ -63,14 +71,15 @@ if qpdf --is-encrypted "$DOCUMENT_WORKING_PATH"; then
                 continue
                 ;;
         esac
-    done < <(printf '%s' ${PWD_CORPUS})
+    done < <(printf '%s' "${PWD_CORPUS}")
     
-    if [[ ${DECODED} -ne 1 ]]; then
-        printf '\ndecryption attempt failed, no password entry matches\n'
+    if (( decoded != 1 )); then
+        printf 'failed\n'
+        printf 'no password entry matches\n'
     fi
 
 else
-    printf '%s is unencrypted, nothing to do\n' ${CONSUMABLE}
+    printf '%s is unencrypted, nothing to do\n' "${CONSUMABLE}"
 fi
 
 printf -- '--- %(%F %H:%M:%S)T -------------------------------------\n' -1
